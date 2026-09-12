86cc93cee9aa0b63afe4d457616a10af98140f7a
sonusharma6-dsa <sonusharma6-dsa@users.noreply.github.com>
2026-09-12 15:22:45 +0530

    Use module.exports CommonJS syntax in api/query.js for Vercel Node.js function

--- api/query.js
+++ api/query.js
@@ -1,4 +1,4 @@
-export default function handler(req, res) {
+module.exports = (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -55,4 +55,4 @@ export default function handler(req, res) {
         execution_trace: trace,
         execution_time_sec: 0.04
     });
-}
+};
